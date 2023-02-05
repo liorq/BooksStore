b475ed049ae024741b497cedaff7cbc7ac9f4f9e
lior daniel <liorqm@gmail.com>
2023-02-05 14:25:44 +0200

    rggr

--- src/app/app.forms.ts
+++ src/app/app.forms.ts
@@ -6,8 +6,8 @@ import Swal from "sweetalert2"
       title: `${title}`,
           html:
             `<div>Please enter  ${property=='user'?'your new userName':'the name of the new book.'}.</div>  <input id="swal-input1" class="swal2-input" value=${property=='user'?parameter.email:parameter.name}>` +
-            `<div>Please enter  ${property=='user'?'your old password':'the new price of the book.'}.</div> <input id="swal-input2" class="swal2-input" value=${property=='user'?'':parameter.price}>` +
-            `<div>Please enter  ${property=='user'?'your new password':'the new author name for the book.'}.</div>   <input id="swal-input3" class="swal2-input"  value=${property=='user'?'':parameter.author}>` ,
+            `<div>Please enter  ${property=='user'?'your old password':'the new price of the book.'}.</div> <input id="swal-input2" type=${property=='user'?'password':'text'} class="swal2-input" value=${property=='user'?'':parameter.price}>` +
+            `<div>Please enter  ${property=='user'?'your new password':'the new author name for the book.'}.</div>   <input id="swal-input3" type=${property=='user'?'password':'text'} class="swal2-input"  value=${property=='user'?'':parameter.author}>` ,
           focusConfirm: false,
           preConfirm: () => {
             return [
