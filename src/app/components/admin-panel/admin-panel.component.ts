660637854adad715da322bb89e923853ee7cc0cd
lior daniel <liorqm@gmail.com>
2023-02-05 18:23:46 +0200

    grgr

--- src/app/components/admin-panel/admin-panel.component.ts
+++ src/app/components/admin-panel/admin-panel.component.ts
@@ -32,13 +32,13 @@ export class AdminPanelComponent implements OnInit{
    constructor(private localService:LocalService,private userInfoService:UserInfoService){}
 
    addBook(){
-    
    const newBook:any= this.createNewBook();
    this.booksToDisplay.push(newBook);
    this.localService.setLocalProperty('allBooks',JSON.stringify([...this.booksToDisplay]))
    Swal.fire(messages.BookAddedToLocalStorage)
    this.resetBookFields()
   }
+
   resetBookFields(){
     this.book={  Book_Name:"",price:"",author_Name:"",Book_Id:"",}
   }
--- src/app/components/my-cart/my-cart.component.ts
+++ src/app/components/my-cart/my-cart.component.ts
@@ -9,58 +9,57 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-my-cart',
   templateUrl: './my-cart.component.html',
-  styleUrls: ['../books-list/books-list.component.css','./my-cart.component.css']
+  styleUrls: [
+    '../books-list/books-list.component.css',
+    './my-cart.component.css',
+  ],
 })
-export class MyCartComponent implements OnInit{
-  constructor(private localService:LocalService,private booksService:BooksService,private userInfoService:UserInfoService){}
-  booksToDisplay:book[]=[];
-  isPaymentModalClose:boolean=true;
-  ngOnInit(){
+export class MyCartComponent implements OnInit {
+  constructor(
+    private localService: LocalService,
+    private booksService: BooksService,
+    private userInfoService: UserInfoService
+  ) {}
+  booksToDisplay: book[] = [];
+  isPaymentModalClose: boolean = true;
+  ngOnInit() {
+    this.userInfoService.isPaymentModalClose.subscribe((newStatus) => {
+      this.isPaymentModalClose = newStatus;
+    });
 
-  this.userInfoService.isPaymentModalClose.subscribe((newStatus)=>{
-  this.isPaymentModalClose=newStatus;
-  })
+    this.booksService.currentBooks.subscribe((updateBooks) => {
+      this.localService.UpdateBooksCartInUsersData([...updateBooks]);
+      this.booksToDisplay = [...updateBooks];
+      this.totalCharge(this.booksToDisplay);
+    });
 
-  this.booksService.currentBooks.subscribe((updateBooks)=>{
-    this.localService.UpdateBooksCartInUsersData([...updateBooks])
-    this.booksToDisplay=[...updateBooks];
-    this.totalCharge(this.booksToDisplay)
-  })
-
-  if (this.localService.isUserLogged())
-  this.UpdateCartFromLocalStorage();
+    if (this.localService.isUserLogged()) this.UpdateCartFromLocalStorage();
   }
 
+  removeBookFromCart(book: book) {
+    const index = this.booksToDisplay.findIndex((b) => b == book);
+    this.booksToDisplay[index].amount == 1? this.booksToDisplay.splice(index, 1)
+      : this.booksToDisplay[index].amount--;
 
-  removeBookFromCart(book:book){
-
-   const index= this.booksToDisplay.findIndex((b)=>b==book)
-   if(this.booksToDisplay[index].amount==1)
-    this.booksToDisplay.splice(index,1)
-   else
-   this.booksToDisplay[index].amount--;
-
-   Swal.fire(messages.BookRemoved);
-    this.booksService.currentBooks.next([...this.booksToDisplay])
+    Swal.fire(messages.BookRemoved);
+    this.booksService.currentBooks.next([...this.booksToDisplay]);
   }
-  AddQuantity(book:book){
+
+  AddQuantity(book: book) {
     book.amount++;
-    this.booksService.currentBooks.next([...this.booksToDisplay])
-    // Swal.fire(messages.BookAdded);
-
+    this.booksService.currentBooks.next([...this.booksToDisplay]);
   }
 
-  displayModalPayment(){
+  displayModalPayment() {
     this.userInfoService.isPaymentModalClose.next(false);
   }
 
-  UpdateCartFromLocalStorage(){
+  UpdateCartFromLocalStorage() {
     const data = this.localService.getBooksInCarts();
     this.booksService.currentBooks.next([...data]);
     this.userInfoService.isUserLogged.next(true);
-}
-  totalCharge(currentCart:book[]){
-    return currentCart.reduce((sum,book) => sum + book.price*book.amount, 0);
   }
-
+  totalCharge(currentCart: book[]) {
+    return currentCart.reduce((sum, book) => sum + book.price * book.amount, 0);
+  }
 }
