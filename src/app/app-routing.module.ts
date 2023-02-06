467d1658b904bda24fff293fc4234cf21614f19e
lior daniel <liorqm@gmail.com>
2023-02-06 16:14:15 +0200

    rgrg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: 'SignUp', component: SignUpComponent },
-  {path: 'users/:user/Setting', component: MySettingsComponent},
+  {path: 'users/:user/Setting', component: MySettingsComponent} ,
   {path: 'users/:user/admin', component: AdminPanelComponent},
   { path: 'users/:user/allBooks', component: AllBooksComponent },
   { path: 'users/:user/myCart', component: MyCartComponent },
