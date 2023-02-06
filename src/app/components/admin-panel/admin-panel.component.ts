f8d9b72ec9fffd25b2abe245044e42cbcfbbb502
lior daniel <liorqm@gmail.com>
2023-02-06 14:08:11 +0200

    grgr

--- src/app/components/admin-panel/admin-panel.component.ts
+++ src/app/components/admin-panel/admin-panel.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { getAllBooks } from 'src/app/app.books';
 import {  getEditBookForm } from 'src/app/app.forms';
-import { book } from 'src/app/app.interfaces';
+import { book, user } from 'src/app/app.interfaces';
 import { messages } from 'src/app/app.messages';
+import { BooksService } from 'src/app/service/books.service';
 import { LocalService } from 'src/app/service/local.service';
 import { UserInfoService } from 'src/app/service/user-info.service';
 import Swal from 'sweetalert2';
@@ -24,12 +25,13 @@ export class AdminPanelComponent implements OnInit{
     author_Name:"",
   }
   ngOnInit(){
-
+    this.booksService.usersData=JSON.parse(this.localService.getLocalProperty('usersData')||"[]")
     this.booksToDisplay=JSON.parse(this.localService.getLocalProperty('allBooks')||"[]")
      this.localService.setLocalProperty('allBooks',JSON.stringify(getAllBooks()))
+
   }
 
-   constructor(private localService:LocalService,private userInfoService:UserInfoService){}
+   constructor(private localService:LocalService,private userInfoService:UserInfoService,public booksService:BooksService){}
 
    addBook(){
    const newBook:any= this.createNewBook();
@@ -56,8 +58,16 @@ export class AdminPanelComponent implements OnInit{
   deleteBook(book:book){
     this.booksToDisplay = this.booksToDisplay.filter(b => b.name !== book.name);
     this.localService.setLocalProperty('allBooks',JSON.stringify(this.booksToDisplay))
+    this.deleteRemovedBookFromAllCarts(book)
+
   }
 
+
+  deleteRemovedBookFromAllCarts(book:book){
+    this.booksService.usersData.forEach((u) =>{
+      return u.booksInCart=u.booksInCart.filter((b) => b.name !== book.name);
+      });
+  }
 
    async editBook(book:book){
     const form=getEditBookForm(book,'Edit book','book')
